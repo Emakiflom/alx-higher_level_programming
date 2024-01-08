d49e546129e34f5b4b1f4eeb30a48cd5f19cb938
Emakiflom <amankflom23@gmail.com>
2024-01-09 00:56:48 +0300

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry for this project."""
+
+
+class BaseGeometry:
+    """Represent base geometry class representation."""
+
+    def area(self):
+        """Not implemented aria function."""
+        raise Exception("area() is not implemented")
